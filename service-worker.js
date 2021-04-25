3e7b028a351c77f05c1767e678f16ee88d11b3d6
w.hunt <wfh2d88@gmail.com>
2021-04-25 09:23:19 -0500

    for askBCS

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,6 @@
-const APP_PREFIX = "FoodFest-";
+const APP_PREFIX = "FoodEvent-";
 const VERSION = "version_01";
 const CACHE_NAME = APP_PREFIX + VERSION;
-
 const FILES_TO_CACHE = [
   "./index.html",
   "./events.html",
@@ -16,35 +15,7 @@ const FILES_TO_CACHE = [
   "./dist/schedule.bundle.js",
 ];
 
-self.addEventListener("install", function (e) {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      console.log("installing cache : " + CACHE_NAME);
-      return cache.addAll(FILES_TO_CACHE);
-    })
-  );
-});
-
-self.addEventListener("activate", function (e) {
-  e.waitUntil(
-    caches.keys().then(function (keyList) {
-      let cacheKeeplist = keyList.filter(function (key) {
-        return key.indexOf(APP_PREFIX);
-      });
-      cacheKeeplist.push(CACHE_NAME);
-
-      return Promise.all(
-        keyList.map(function (key, i) {
-          if (cacheKeeplist.indexOf(key) === -1) {
-            console.log("deleting cache : " + keyList[i]);
-            return caches.delete(keyList[i]);
-          }
-        })
-      );
-    })
-  );
-});
-
+// Respond with cached resources
 self.addEventListener("fetch", function (e) {
   console.log("fetch request : " + e.request.url);
   e.respondWith(
@@ -61,6 +32,40 @@ self.addEventListener("fetch", function (e) {
 
       // You can omit if/else for console.log & put one line below like this too.
       // return request || fetch(e.request)
+    })
+  );
+});
+
+// Cache resources
+self.addEventListener("install", function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log("installing cache : " + CACHE_NAME);
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+// Delete outdated caches
+self.addEventListener("activate", function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      // `keyList` contains all cache names under your username.github.io
+      // filter out ones that has this app prefix to create keeplist
+      let cacheKeeplist = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX);
+      });
+      // add current cache name to keeplist
+      cacheKeeplist.push(CACHE_NAME);
+
+      return Promise.all(
+        keyList.map(function (key, i) {
+          if (cacheKeeplist.indexOf(key) === -1) {
+            console.log("deleting cache : " + keyList[i]);
+            return caches.delete(keyList[i]);
+          }
+        })
+      );
     })
   );
 });
